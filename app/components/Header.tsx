3cc5844f83d29d1121c2881e833bd024cf8a9bbf
nathantmckenzie <nathantaylormckenzie@hotmail.com>
2026-03-11 08:55:22 -0700

    .

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
-import PriorityNav from "./PriorityNav";
 
 export default function Header() {
   return (
